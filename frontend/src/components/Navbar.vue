bf966a5e7214bf52e9192b80d23a7a5d3774c8c0
AnthonyBeckett <antneyb10@gmail.com>
2024-09-05 17:30:32 +0100

    Updated and tested routing. Few more small tweaks on the navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,13 +6,13 @@
             </template>
 
             <template #item="{ item, props, hasSubmenu, root }">
-                <a v-ripple class="flex items-center" v-bind="props.action">
+                <router-link :to="item.to" v-ripple class="flex items-center" v-bind="props.action">
                     <span :class="item.icon" />
                     <span class="ml-1">{{ item.label }}</span>
                     <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
                     <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
                     <i v-if="hasSubmenu" :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
-                </a>
+                </router-link>
             </template>
 
             <template #end>
@@ -33,11 +33,13 @@ import Logo from "@/components/assets/Logo.vue";
 const items = ref([
     {
         label: 'Dashboard',
-        icon: 'pi pi-home'
+        icon: 'pi pi-home',
+        to: '/'
     },
     {
-        label: 'Features',
-        icon: 'pi pi-star'
+        label: 'Test',
+        icon: 'pi pi-star',
+        to: '/test'
     },
 ]);
 </script>
--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -5,7 +5,7 @@
 <template>
   <main>
       <h1>
-          Home view
+          Test
       </h1>
   </main>
 </template>
